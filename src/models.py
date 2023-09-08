22f34f6fe5101f19d044e186e32a1e113ab36352
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-09-08 15:41:34 +0000

    diagrama hecho

--- src/models.py
+++ src/models.py
@@ -7,23 +7,52 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Person(Base):
+    __tablename__ = 'person'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class FavoritePlanet(Base):
+    __tablename__ = 'favorite_planet'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+   
+class FavoritePerson(Base):
+    __tablename__ = 'favorite_person'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
     person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+   
+class FavoriteStarship(Base):
+    __tablename__ = 'favorite_starship'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    starship_id = Column(Integer, ForeignKey('starship.id'))
+    
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    favoriteStarshipId = Column(Integer, ForeignKey('favorite_starship.id'))
+    favoritePersonId = Column(Integer, ForeignKey('favorite_person.id'))
+    favoritePlanetId = Column(Integer, ForeignKey('favorite_planet.id'))
+
 
     def to_dict(self):
         return {}
