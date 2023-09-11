e3a650d70f0de38eaf4c20735c92681885f8d041
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-09-11 00:26:51 +0000

    solucionado propiedades y tabla favs unica

--- src/models.py
+++ src/models.py
@@ -11,47 +11,44 @@ class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    
 
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+
 
 class Person(Base):
     __tablename__ = 'person'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    hair_color=Column(String(250), nullable=True)
+    gender=Column(String(250), nullable=True)
+    birth_year=Column(String(250), nullable=False)
+
 
 class Starship(Base):
     __tablename__ = 'starship'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    passengers = Column(String(250), nullable=False)
+    max_atmosphering_speed = Column(String(250), nullable=False)
 
-class FavoritePlanet(Base):
-    __tablename__ = 'favorite_planet'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    planet_id = Column(Integer, ForeignKey('planet.id'))
-   
-class FavoritePerson(Base):
-    __tablename__ = 'favorite_person'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    person_id = Column(Integer, ForeignKey('person.id'))
-   
-class FavoriteStarship(Base):
-    __tablename__ = 'favorite_starship'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    starship_id = Column(Integer, ForeignKey('starship.id'))
+
     
 class Favorite(Base):
     __tablename__ = 'favorite'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    favoriteStarshipId = Column(Integer, ForeignKey('favorite_starship.id'))
-    favoritePersonId = Column(Integer, ForeignKey('favorite_person.id'))
-    favoritePlanetId = Column(Integer, ForeignKey('favorite_planet.id'))
+    favoriteStarshipId = Column(Integer, ForeignKey('starship.id'), nullable=True)
+    favoritePersonId = Column(Integer, ForeignKey('person.id'), nullable=True)
+    favoritePlanetId = Column(Integer, ForeignKey('planet.id'), nullable=True)
 
 
     def to_dict(self):
@@ -59,3 +56,21 @@ class Favorite(Base):
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+# class FavoritePlanet(Base):
+#     __tablename__ = 'favorite_planet'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('user.id'))
+#     planet_id = Column(Integer, ForeignKey('planet.id'))
+   
+# class FavoritePerson(Base):
+#     __tablename__ = 'favorite_person'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('user.id'))
+#     person_id = Column(Integer, ForeignKey('person.id'))
+   
+# class FavoriteStarship(Base):
+#     __tablename__ = 'favorite_starship'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('user.id'))
+#     starship_id = Column(Integer, ForeignKey('starship.id'))
